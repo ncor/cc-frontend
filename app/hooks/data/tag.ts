1b60149526cf411191729f0a611da9b79d8cca1c
ensi <66754841+ncor@users.noreply.github.com>
2023-11-22 07:47:10 +0300

    moved hooks

--- app/hooks/data/tag.ts
+++ app/hooks/data/tag.ts
@@ -1,39 +1,38 @@
 'use client';
 
 import { useToast } from "@/components/ui/use-toast";
-import { useContext } from "react";
-import { sessionContext } from "../../contexts/session";
 import { ArgsType } from "@/lib/types";
 import { createTag, findTag, deleteTag, updateTag } from "@/lib/tag/endpoints";
 import { tagPolicy } from "@/lib/tag/policy";
 import { Tag } from "@/lib/tag/types";
 import { User } from "@/lib/user/types";
 import cook from "../cook";
+import useUser from "../user";
 
 
 export default function useTags() {
     const toast = useToast();
-    const session = useContext(sessionContext);
+    const user = useUser();
 
     const create = async (args: ArgsType<typeof createTag>[1]) =>
-        cook(toast, await createTag(session, args));
+        cook(toast, await createTag(user, args));
 
     const find = async (args: ArgsType<typeof findTag>[1]) =>
-        cook(toast, await findTag(session, {
+        cook(toast, await findTag(user, {
             ...args
         }));
 
     const findOwn = async(args: ArgsType<typeof findTag>[1]) =>
-        find({ ...args, where: { ...args.where, owner_id: session.user.id } });
+        find({ ...args, where: { ...args.where, owner_id: user.id } });
 
     const findPublic = async(args: ArgsType<typeof findTag>[1]) =>
         find({ ...args, where: { ...args.where, is_public: true } });
 
     const update = async (args: ArgsType<typeof updateTag>[1]) =>
-        cook(toast, await updateTag(session, args));
+        cook(toast, await updateTag(user, args));
 
     const remove = async (args: ArgsType<typeof deleteTag>[1]) =>
-        cook(toast, await deleteTag(session, args));
+        cook(toast, await deleteTag(user, args));
 
     const can = (action: string, tag: Tag, user: User) =>
         tagPolicy.isAllowed(action, tag, user)

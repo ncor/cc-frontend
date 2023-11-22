4dca8acf8b847072d630df9dc5d3d87ae5bc46c4
ensi <66754841+ncor@users.noreply.github.com>
2023-11-22 07:46:36 +0300

    deprecated (supabase & etc.)

--- app/hooks/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-
-export async function useServerSupabase() {
-    return createServerComponentClient({ cookies });
-}

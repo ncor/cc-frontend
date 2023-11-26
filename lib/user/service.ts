3ec2b8e439359f10b253223668b60346806c836e
ensi <66754841+ncor@users.noreply.github.com>
2023-11-26 18:12:59 +0300

    removed deprecated functions, adapted to new schema

--- lib/user/service.ts
+++ lib/user/service.ts
@@ -1,35 +1,11 @@
-import { Prisma } from "@prisma/client";
-import { ROLES } from "./constants";
-import { prisma } from "../prisma";
-import { ERRORS } from "../error/constants";
 import { User } from "./types";
+import { ERRORS } from "../error/constants";
 
-
-export const isAdmin = (roles: string[]) => {
-    return roles.includes(ROLES.ADMIN);
-}
-
-export const getUserAdapter = async (args: Prisma.userFindFirstArgs) => {
-    const user = await prisma.user.findFirst(args);
-    if (!user) throw ERRORS.USER.NOT_EXISTS;
-    return user as User;
-}
 
 export const verifyUserUpdateAccess = async (
     user: User,
     targetUser: User
 ) => {
-    if (user != targetUser && !isAdmin(user.roles))
+    if (user.id != targetUser.id && user.is_admin)
         throw ERRORS.AUTH.NOT_PERMITTED;
 }
-
-export const verifyRolesModifyAccess = async (
-    userRoles: string[],
-    targetUserRoles: string[]
-) => {
-    const isUserAdmin = isAdmin(userRoles);
-    const isTargetUserAdmin = isAdmin(targetUserRoles);
-
-    if (!isUserAdmin || isUserAdmin == isTargetUserAdmin)
-        throw ERRORS.AUTH.NOT_PERMITTED;
-};

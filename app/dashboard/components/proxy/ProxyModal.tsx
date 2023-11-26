1b62f87dace4e7988d09ebc868798ad98b3345a7
ensi <66754841+ncor@users.noreply.github.com>
2023-11-26 18:18:43 +0300

    added user chip

--- app/dashboard/components/proxy/ProxyModal.tsx
+++ app/dashboard/components/proxy/ProxyModal.tsx
@@ -4,9 +4,14 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { useState } from "react";
 import { ModalProps } from "@/lib/client/types";
 import ProxyForm, { ProxyFormProps } from "./ProxyForm";
+import { User } from "@/lib/user/types";
+import { Proxy } from "@/lib/proxy/types";
+import UserChip from "../user/UserChip";
 
 
-export type ProxyModalProps = ModalProps & ProxyFormProps;
+export type ProxyModalProps = ModalProps & {
+    update?: Proxy & { user: User }
+};
 
 export default function ProxyModal({
     children, update, onSubmit
@@ -20,7 +25,15 @@ export default function ProxyModal({
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Прокси</DialogTitle>
+                    <DialogTitle className="flex items-center gap-2">
+                        {
+                            update &&
+                            <div className="px-3 py-2 bg-zinc-900 rounded-md">
+                                <UserChip user={ update.user }/>
+                            </div>
+                        }
+                        Прокси
+                    </DialogTitle>
                     <DialogDescription>
                         Прокси используются для анонимизации запросов и обхода лимита частоты запросов.
                     </DialogDescription>

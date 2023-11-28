c74ce9f0682a29d03d8cb1c6f437a575d6feff67
ensi <66754841+ncor@users.noreply.github.com>
2023-11-28 07:46:04 +0300

    implemented ThemeSwitcher

---
+++ app/dashboard/components/header/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import useTheme from "@/app/hooks/theme";
+import { Button } from "@/components/ui/button";
+import { Moon, SunDim } from "lucide-react";
+
+
+export default function ThemeSwitcher() {
+    const { theme, switchTheme } = useTheme();
+
+    const handleToggle = () => {
+        switchTheme(theme == 'dark' ? 'light' : 'dark');
+    };
+
+    return <Button
+        variant="ghost"
+        size="icon"
+        onClick={ () => handleToggle() }
+    >
+        {
+            theme == 'dark'
+            ? <SunDim className="w-5 h-5"/>
+            : <Moon className="w-4 h-4"/>
+        }
+    </Button>
+};

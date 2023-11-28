e8c5946ce4aefd328eecdad4d3614c375b68487e
ensi <66754841+ncor@users.noreply.github.com>
2023-11-28 07:48:49 +0300

    adapted to robust solution

--- app/dashboard/components/header/Navigation.tsx
+++ app/dashboard/components/header/Navigation.tsx
@@ -1,21 +1,57 @@
-'use client';
-
-import useUser from "@/app/hooks/user";
-import NavigationItem from "./NavigationItem";
+import { ListItem } from "@/components/ui/list-item";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link2, Shield } from "lucide-react";
+import Link from "next/link";
+import { forwardRef } from "react";
+import useUser from "../../user/hooks/user";
 
 
 export default function Navigation() {
     const user = useUser();
 
-    return <nav className="flex items-center space-x-4 lg:space-x-6">
-        <NavigationItem href="/dashboard/proxy">
-            Прокси
-        </NavigationItem>
-        {
-            user.is_admin &&
-            <NavigationItem href="/dashboard/users">
-                Пользователи
-            </NavigationItem>
-        }
-    </nav>;
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href="/dashboard/proxy" legacyBehavior passHref>
+                        <NavigationMenuLink
+                            className={navigationMenuTriggerStyle()}
+                        >
+                            Прокси
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                {
+                    user.is_admin &&
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger className="flex items-center gap-1">
+                            <Shield className="w-4 h-4"/>
+                            Пользователи
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid p-3 w-[300px] lg:grid-cols-1">
+                                <ListItem href="/dashboard/users" title="Список пользователей">
+                                    Страница управления пользователями.
+                                </ListItem>
+                                <ListItem
+                                    href="/dashboard/activity"
+                                    title="Активность"
+                                >
+                                    Лог действий и изменений от лиц пользователей.
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                }
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
 }

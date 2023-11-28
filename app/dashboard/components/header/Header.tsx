766f6ee373e36bd07fa6b04d6bca200c8d7f2a06
ensi <66754841+ncor@users.noreply.github.com>
2023-11-28 07:48:01 +0300

    added new features

--- app/dashboard/components/header/Header.tsx
+++ app/dashboard/components/header/Header.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import Navigation from "./Navigation";
-import UserMenu from "../user/UserMenu";
+import UserMenu from "../../users/components/UserMenu";
+import ThemeSwitcher from "./ThemeSwitcher";
+import Image from "next/image";
+import LogoImage from "@/app/components/LogoImage";
 
 
 export default function Header() {
     return (
         <div className="w-screen flex justify-center border-b px-8 pr-6">
-            <div className="w-full max-w-5xl flex items-center h-16">
+            <div className="w-full max-w-5xl flex items-center h-14">
+                <div className="h-8 mr-2 dark:invert">
+                    <LogoImage/>
+                </div>
                 <Navigation/>
-                <UserMenu/>
+                <div className="ml-auto flex items-center space-x-4">
+                    <ThemeSwitcher/>
+                    <UserMenu/>
+                </div>
             </div>
         </div>
     );

f876d626d18353e1a1b8a36b5844001d0b246dcc
ensi <66754841+ncor@users.noreply.github.com>
2023-11-30 08:10:18 +0300

    refactored user page

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -2,29 +2,41 @@ import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import UserModal from "./components/UserModal";
 import UserPageStats from "./components/UserPageStats";
-import Heading from "../components/heading/Heading";
-import HeadingTitle from "../components/heading/HeadingTitle";
-import HeadingControls from "../components/heading/HeadingControls";
-import UserTableSection from "./components/UserTableSection";
 import PageContent from "../components/PageContent";
+import Banner from "../components/heading/Banner";
+import BannerControls from "../components/heading/BannerControls";
+import BannerTitle from "../components/heading/BannerTitle";
+import BannerHeader from "../components/heading/BannerHeader";
+import BannerDescription from "../components/heading/BannerDescription";
+import RevalidationButton from "../components/RevalidationButton";
+import UserTable from "./components/UserTable";
+import TableSection from "../components/table/TableSection";
 
 
 export default async function UsersPage() {
     return <PageContent>
-        <Heading>
-            <HeadingTitle includeRevalidation>
-                Пользователи
-            </HeadingTitle>
-            <HeadingControls>
+        <Banner>
+            <BannerHeader>
+                <BannerTitle>
+                    Пользователи
+                </BannerTitle>
+                <BannerDescription>
+                    Управление пользователями.
+                </BannerDescription>
+            </BannerHeader>
+            <BannerControls>
+                <RevalidationButton/>
                 <UserModal>
                     <Button>
                         <Plus className="w-4 h-4 mr-2"/>
                         Зарегистрировать
                     </Button>
                 </UserModal>
-            </HeadingControls>
-        </Heading>
+            </BannerControls>
+        </Banner>
         <UserPageStats/>
-        <UserTableSection/>
+        <TableSection>
+            <UserTable/>
+        </TableSection>
     </PageContent>
 }

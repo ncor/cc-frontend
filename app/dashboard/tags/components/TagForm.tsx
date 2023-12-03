9ca73488df697605c4da85e07ba4ab301944b9f2
ensi <66754841+ncor@users.noreply.github.com>
2023-12-03 20:20:20 +0300

    implement upsert functionality

---
+++ app/dashboard/tags/components/TagForm.tsx
@@ -0,0 +1,84 @@
+import { z } from "zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import useTags from "../hooks/data/tag";
+import PermissionsFormFields from "../../components/resource/permissions/PermissionsFormFields";
+import useUser from "../../users/hooks/user";
+import { Tag } from "@/lib/tag/types";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import { useToast } from "@/components/ui/use-toast";
+import useUpsertForm from "@/app/hooks/upsert-form";
+
+
+export const tagSchema = z.object({
+    name: z.string().min(2).max(50),
+    owner_id: z.string(),
+    is_public: z.boolean(),
+});
+
+export type TagSchemaType = z.infer<typeof tagSchema>;
+
+export interface TagFormProps {
+    reference?: Tag,
+    onSubmit?: <T>(form: T) => void;
+}
+
+export default function TagForm({
+    reference, onSubmit
+}: TagFormProps) {
+    const user = useUser();
+    const { update: updateTag, create: createTag } = useTags();
+
+    const { form, submit, isLoading } = useUpsertForm({
+        schema: tagSchema,
+        defaults: {
+            name: '',
+            is_public: false,
+            owner_id: user.id
+        },
+        updateCallback: updateTag,
+        insertCallback: createTag,
+        updateReference: reference,
+        onSubmit
+    });
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(submit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Имя тега</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Имя тега" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    disabled={isLoading}
+                />
+                {
+                    user.is_admin &&
+                    <PermissionsFormFields
+                        form={ form }
+                        disabled={ isLoading }
+                    />
+                }
+                <Button type="submit" className="w-full" disabled={ isLoading }>
+                    <LoadingSpinner isLoading={ isLoading }/>
+                    Сохранить
+                </Button>
+            </form>
+        </Form>
+    );
+}
---
+++ app/dashboard/tags/components/TagModal.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import TagForm from "./TagForm";
+import { ModalProps } from "@/lib/client/types";
+import { TagExtended } from "@/lib/tag/types";
+import { VisibilityInterface } from "@/app/hooks/visibility";
+import UserChip from "../../users/components/UserChip";
+
+
+export type TagModalProps = ModalProps & {
+    visibility: VisibilityInterface,
+    reference?: TagExtended
+};
+
+export default function TagModal({
+    visibility, reference, onSubmit
+}: TagModalProps) {
+    return <Dialog
+        open={ visibility.isVisible }
+        onOpenChange={ open => visibility.toggle(open) }
+    >
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle className="flex items-center gap-2">
+                    {
+                        reference &&
+                        <UserChip user={ reference.user } card/>
+                    }
+                    Тег
+                </DialogTitle>
+                <DialogDescription>
+                    Теги служат для маркировки аккаунтов и прокси, упрощая организацию и поиск.
+                </DialogDescription>
+            </DialogHeader>
+            <TagForm
+                reference={ reference }
+                onSubmit={ form => {
+                    visibility.toggle(false);
+                    onSubmit && onSubmit(form);
+                } }
+            />
+        </DialogContent>
+    </Dialog>
+}

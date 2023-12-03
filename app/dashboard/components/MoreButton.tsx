65d5db76bb8ba6d000a6d170d014b92bd5e3dd6a
ensi <66754841+ncor@users.noreply.github.com>
2023-12-03 20:13:21 +0300

    implemented MoreButton

---
+++ app/dashboard/components/MoreButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+import { ButtonHTMLAttributes } from "react";
+
+
+export default function MoreButton(props: ButtonHTMLAttributes<any>) {
+    return <Button
+        variant="ghost"
+        className="h-8 w-8 p-0"
+        { ...props }
+    >
+        <MoreHorizontal className="h-4 w-4" />
+    </Button>;
+}

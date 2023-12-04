a4eef731eb0389c360685fde9427151e77b30a34
ensi <66754841+ncor@users.noreply.github.com>
2023-12-04 21:51:32 +0300

    added overflow feature

--- app/dashboard/components/table/TableTagsCell.tsx
+++ app/dashboard/components/table/TableTagsCell.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { Badge } from "@/components/ui/badge";
 import { TableCell } from "@/components/ui/table";
 import { Minus } from "lucide-react";
+import RevealExtraButton from "../RevealExtraButton";
 
+
+export const MAX_VISIBLE_TAGS = 1;
 
 export interface TableTagsCellProps {
     tags: string[]
@@ -10,17 +15,29 @@ export interface TableTagsCellProps {
 export default function TableTagsCell({
     tags
 }: TableTagsCellProps) {
-    return <TableCell className="space-x-1">
+    const tagsList = (tags: string[]) => tags.map((tag, i) =>
+        <Badge
+            key={ i }
+            variant="secondary"
+        >
+            {tag}
+        </Badge>
+    );
+
+    return <TableCell>
         {
             tags.length
-            ? tags.map((tag, i) => (
-                <Badge
-                    key={ i }
-                    variant="secondary"
-                >
-                    {tag}
-                </Badge>
-            ))
+            ? <div className="flex items-center gap-1 flex-wrap">
+                { tagsList(tags.slice(0, MAX_VISIBLE_TAGS)) }
+                {
+                    (tags.length - MAX_VISIBLE_TAGS) > 0 &&
+                    <RevealExtraButton>
+                        <div className="flex flex-col gap-1">
+                            { tagsList(tags.slice(MAX_VISIBLE_TAGS)) }
+                        </div>
+                    </RevealExtraButton>
+                }
+            </div>
             : <Minus className="w-4 h-4 text-muted-foreground"/>
         }
     </TableCell>
